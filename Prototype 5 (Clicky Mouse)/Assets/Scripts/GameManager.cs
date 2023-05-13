456fd81e6fc750b9c7806c5c38c067e19d590d9b
Ivan-Lanin <ivanandrlanin@gmail.com>
2023-05-13 21:59:19 +0200

    V0.12
    Restart feature

--- Prototype 5 (Clicky Mouse)/Assets/Scripts/GameManager.cs
+++ Prototype 5 (Clicky Mouse)/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
@@ -10,10 +12,13 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 2.0f;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     private int score;
 
 
     private void Start() {
+        isGameActive = true;
         StartCoroutine("SpawnTarget");
         score = 0;
         UpdateScore(0);
@@ -21,7 +26,7 @@ public class GameManager : MonoBehaviour
 
 
     IEnumerator SpawnTarget() {
-        while (true) {
+        while (isGameActive) {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
@@ -36,6 +41,13 @@ public class GameManager : MonoBehaviour
 
 
     public void GameOver() {
+        isGameActive = false;
         gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
